5aa505077a2d1385590e9afa479ee803f135d599
Jimmy <jimmy.scionti@gmail.com>
2019-07-21 22:12:56 +0200

    Add files via upload

---
+++ .JustTheme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# edit the following to set different themes and variants
+gnome_theme_light="Adwaita-Light";
+gnome_theme_dark="Adwaita-Dark";
+gedit_theme_light="classic";
+gedit_theme_dark="oblivion";
+
+while true; do
+
+	clear;
+	
+	nightmode="$(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled)";
+	
+	if [ "$nightmode" == "true" ]; then
+		$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_dark" );
+		$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_dark" );
+	else
+		$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_light" );
+		$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_light" );
+	fi
+	
+	sleep 300;
+
+done
