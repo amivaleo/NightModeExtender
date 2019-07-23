4aa81e95c53d8895fc9294923d4eb7f5fd717996
Jimmy <jimmy.scionti@gmail.com>
2019-07-23 21:39:55 +0200

    Update .JustTheme.sh

--- .JustTheme.sh
+++ .JustTheme.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-# edit the following to set different themes and variants
+# leave it empty if you want to use adwaita theme
 gnome_theme_light="Adwaita";
 gnome_theme_dark="Adwaita-dark";
 gedit_theme_light="classic";
 gedit_theme_dark="oblivion";
 
+# default values
+lastmonth=0;
+lastday=0;
+currentdday=$(date +%d);
+currentmonth=$(date +%m);
+currenttime=$(date +%H:%M:%S);
 
-while true; do
-
+while true; do 
+	
 	clear;
 	
-	nightmode="$(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled)";
+	if [[ "$gnome_theme_light" == "" ]] || [[ "$gnome_theme_dark" == "" ]]; then
+		gnome_theme_light="Adwaita";
+		gnome_theme_dark="Adwaita-dark";
+	fi
 	
-	if [ "$nightmode" == "true" ]; then
+	if [[ "$lastmonth" != "$currentmonth" ]] && [[ "$lastday" != "$currentday" ]]; then
+		lastmonth="$currentmonth";
+		lastday="$currentday";
+		
+		get="$(curl -s "http://ip-api.com/json")";
+		lon=$(echo $get | jq -r '.lon');
+		lat=$(echo $get | jq -r '.lat');
+
+		get2="$(curl -s "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lon&formatted=0")";
+		sunrise=$(echo $get2 | jq -r '.results.sunrise');
+		sunset=$(echo $get2 | jq -r '.results.sunset');
+		sunrise="$(echo $sunrise |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
+		sunset="$(echo $sunset |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
+	fi
+	
+	if [[ "$currenttime" > "$sunset" ]] || [[ "$currenttime" < "$sunrise" ]]; then
 		$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_dark" );
 		$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_dark" );
 	else
@@ -22,5 +46,5 @@ while true; do
 	fi
 	
 	sleep 300;
-
+	
 done
