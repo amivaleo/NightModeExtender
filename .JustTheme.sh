5391c2775d181a211398b71ba02ca833ffa7b1ba
Jimmy <jimmy.scionti@gmail.com>
2019-07-26 14:04:46 +0200

    Update .JustTheme.sh

--- .JustTheme.sh
+++ .JustTheme.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # leave it empty if you want to use adwaita theme
-gnome_theme_light="Adwaita";
-gnome_theme_dark="Adwaita-dark";
+gnome_theme_light="Adwaita-Light";
+gnome_theme_dark="Adwaita-Dark";
 gedit_theme_light="classic";
 gedit_theme_dark="oblivion";
+gimp_theme_light="Symbolic-Inverted";
+gimp_theme_dark="Symbolic";
 
 # default values
 lastmonth=0;
@@ -33,6 +35,7 @@ while true; do
 		sunset=$(echo $get2 | jq -r '.results.sunset');
 		sunrise="$(echo $sunrise |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
 		sunset="$(echo $sunset |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
+		
 	fi
 	
 	currenttime=$(date -u +%H:%M:%S);
@@ -40,9 +43,22 @@ while true; do
 	if [[ "$currenttime" > "$sunset" ]] || [[ "$currenttime" < "$sunrise" ]]; then
 		$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_dark" );
 		$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_dark" );
+		
+		if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
+			sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_dark'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
+		else
+			sed -i 's/(icon-size/(icon-theme "'$gimp_theme_dark'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
+		fi
+		
 	else
 		$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_light" );
 		$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_light" );
+		
+		if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
+			sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_light'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
+		else
+			sed -i 's/(icon-size/(icon-theme "'$gimp_theme_light'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
+		fi
 	fi
 	
 	sleep 300;
