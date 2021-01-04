0fefccb1b77d255d0d8d93f9c432fc8b3ef9c6d6
amivaleo <jimmy.scionti@gmail.com>
2021-01-04 10:05:46 +0100

    only check calendar at startup

--- .JustTheme.sh
+++ .JustTheme.sh
@@ -15,104 +15,86 @@ gnome_latex_theme_dark="oblivion";
 # default values
 lastmonth=0;
 lastday=0;
+sunrise=25200;
+sunset=64800;
 
 # check internet connection
 wget -q --spider http://google.com
 
-# if online 
-if [ $? -eq 0 ]; then
-
-	while true; do 
+	if [ $? -eq 0 ]; then
 		
-		currenttheme=$(gsettings get org.gnome.desktop.interface gtk-theme);
-		currentdday=$(date -u +%d);
-		currentmonth=$(date -u +%m);
+	if [[ "$lastmonth" != "$currentmonth" ]] || [[ "$lastday" != "$currentday" ]]; then
+		lastmonth="$currentmonth";
+		lastday="$currentday";
 		
-		if [[ "$lastmonth" != "$currentmonth" ]] || [[ "$lastday" != "$currentday" ]]; then
-	#		notify-send -i "SUMMARY" "$lastmonth $currentmonth - $lastday $currentday";
-			lastmonth="$currentmonth";
-			lastday="$currentday";
-			
-			get="$(curl -s "http://ip-api.com/json")";
-			lon=$(echo $get | jq -r '.lon');
-			lat=$(echo $get | jq -r '.lat');
-			
-			get2="$(curl -s "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lon&formatted=0")";
-			sunrise=$(echo $get2 | jq -r '.results.sunrise');
-			sunset=$(echo $get2 | jq -r '.results.sunset');
-			
-			sunrise="$(echo $sunrise |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
-			sunset="$(echo $sunset |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
-
-			sunrise=$(echo $sunrise | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
-			sunset=$(echo $sunset | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
-			
-			sunrise=$((sunrise+3600))
-			sunset=$((sunset+3600))
-			
-		fi
+		get="$(curl -s "http://ip-api.com/json")";
+		lon=$(echo $get | jq -r '.lon');
+		lat=$(echo $get | jq -r '.lat');
 		
-		eval "$(date +'today=%F now=%s')"
-		midnight=$(date -d "$today 0" +%s)
-		currenttime=$((now - midnight))
-
-		echo sunrise = $sunrise
-		echo currenttime = $currenttime
-		echo sunset = $sunset
+		get2="$(curl -s "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lon&formatted=0")";
+		sunrise=$(echo $get2 | jq -r '.results.sunrise');
+		sunset=$(echo $get2 | jq -r '.results.sunset');
 		
-		# during the day:
-		if [[ "$sunrise" < "$currenttime" ]] && [[ "$currenttime" < "$sunset" ]] ; then
+		sunrise="$(echo $sunrise |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
+		sunset="$(echo $sunset |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}')";
 
-			if [[ "$currenttheme" != "'$gnome_theme_light'" ]]; then
-				$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_light" );
-				$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_light" );
-				$( gsettings set org.gnome.gnome-latex.preferences.editor scheme "$gnome_latex_theme_light" );
-				
-				echo "0" | sudo tee /sys/devices/platform/dell-laptop/leds/dell::kbd_backlight/brightness > /dev/null;
-				sed -i '/SymbolStyle/c\<item oor:path="/org.openoffice.Office.Common/Misc"><prop oor:name="SymbolStyle" oor:op="fuse"><value>'$libreoffice_theme_light'</value></prop></item>' ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu
-				
-				if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
-					sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_light'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
-				else
-					sed -i 's/(icon-size/(icon-theme "'$gimp_theme_light'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
-				fi
-			fi
+		sunrise=$(echo $sunrise | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }');
+		sunset=$(echo $sunset | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }');
 		
-		# during the day
-		else
-
-			if [[ "$currenttheme" != "$gnome_theme_dark" ]]; then
-				$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_dark" );
-				$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_dark" );
-				$( gsettings set org.gnome.gnome-latex.preferences.editor scheme "$gnome_latex_theme_dark" );
-				
-				echo "1" | sudo tee /sys/devices/platform/dell-laptop/leds/dell::kbd_backlight/brightness > /dev/null;
-				sed -i '/SymbolStyle/c\<item oor:path="/org.openoffice.Office.Common/Misc"><prop oor:name="SymbolStyle" oor:op="fuse"><value>'$libreoffice_theme_dark'</value></prop></item>' ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu
-				
-				if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
-					sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_dark'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
-				else
-					sed -i 's/(icon-size/(icon-theme "'$gimp_theme_dark'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
-				fi
-			fi
-
-		fi
-		
-		sleep 1800;
-
-	done
-
-# if offline
-else
-	$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_light" );
-	$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_light" );
-	$( gsettings set org.gnome.gnome-latex.preferences.editor scheme "$gnome_latex_theme_light" );
-	
-	sed -i '/SymbolStyle/c\<item oor:path="/org.openoffice.Office.Common/Misc"><prop oor:name="SymbolStyle" oor:op="fuse"><value>'$libreoffice_theme_light'</value></prop></item>' ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu
-
-	if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
-		sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_light'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
-	else
-		sed -i 's/(icon-size/(icon-theme "'$gimp_theme_light'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc
+		sunrise=$((sunrise+3600));
+		sunset=$((sunset+7200));
 	fi
 fi
+
+
+while true; do 
+	
+	currenttheme=$(gsettings get org.gnome.desktop.interface gtk-theme);
+	currentdday=$(date -u +%d);
+	currentmonth=$(date -u +%m);
+		
+	eval "$(date +'today=%F now=%s')";
+	midnight=$(date -d "$today 0" +%s);
+	currenttime=$((now - midnight));
+		
+	# during the day:
+	if [[ "$sunrise" < "$currenttime" ]] && [[ "$currenttime" < "$sunset" ]] ; then
+		
+		if [[ "$currenttheme" != "$gnome_theme_light" ]]; then
+			$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_light" );
+			$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_light" );
+			$( gsettings set org.gnome.gnome-latex.preferences.editor scheme "$gnome_latex_theme_light" );
+			
+			sed -i '/SymbolStyle/c\<item oor:path="/org.openoffice.Office.Common/Misc"><prop oor:name="SymbolStyle" oor:op="fuse"><value>'$libreoffice_theme_light'</value></prop></item>' ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu;
+			
+			if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
+				sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_light'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc;
+			else
+				sed -i 's/(icon-size/(icon-theme "'$gimp_theme_light'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc;
+			fi
+		fi
+	
+	# during the night
+	else
+	
+		if [[ "$currenttheme" != "$gnome_theme_dark" ]]; then
+			$( gsettings set org.gnome.desktop.interface gtk-theme "$gnome_theme_dark" );
+			$( gsettings set org.gnome.gedit.preferences.editor scheme "$gedit_theme_dark" );
+			$( gsettings set org.gnome.gnome-latex.preferences.editor scheme "$gnome_latex_theme_dark" );
+			
+			sed -i '/SymbolStyle/c\<item oor:path="/org.openoffice.Office.Common/Misc"><prop oor:name="SymbolStyle" oor:op="fuse"><value>'$libreoffice_theme_dark'</value></prop></item>' ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu;
+			
+			if [[ "$(grep "icon-theme" ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc)" ]]; then
+				sed -i '/(icon-theme/c\(icon-theme "'$gimp_theme_dark'")' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc;
+			else
+				sed -i 's/(icon-size/(icon-theme "'$gimp_theme_dark'")\n(icon-size/g' ~/.var/app/org.gimp.GIMP/config/GIMP/2.10/gimprc;
+			fi
+		fi
+	fi
+		
+	touch .JustThemeOutput;
+	echo "$sunrise < $currenttime < $sunset" >> .JustThemeOutput;
+
+	sleep 900;
+
+done
